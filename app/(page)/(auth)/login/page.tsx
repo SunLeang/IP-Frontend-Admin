ae26f751bc4897dbef00eb314164171083b8ff35
SunLeang123 <leangkiyan2020@gmail.com>
2025-05-13 14:03:47 +0700

    Design Login Page

--- app/(page)/(auth)/login/page.tsx
+++ app/(page)/(auth)/login/page.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import Image from "next/image"
+import LoginForm from "@/components/login-form"
 
-function page() {
+export default function LoginPage() {
   return (
-    <div>login</div>
+    <div className="flex flex-col min-h-screen w-full bg-[#F1F5F9]">
+      {/* Logo in top left */}
+      <div className="p-6 pt-8">
+        <Image src="/logo.png" alt="eventura" width={120} height={84} priority />
+      </div>
+
+      {/* Login form centered but moved up a bit */}
+      <div className="flex flex-1 justify-center items-center px-6 pb-16 -mt-10">
+        <div className="flex w-full max-w-[780px] bg-white rounded-[20px] overflow-hidden shadow-lg border border-gray-100">
+          <Image
+            src="/loginpic.png"
+            alt="Login background"
+            width={340}
+            height={540}
+            className="w-[340px] object-cover max-lg:hidden"
+            priority
+          />
+
+          <LoginForm />
+        </div>
+      </div>
+    </div>
   )
 }
-
-export default page
